b23bd1d39c4180372aa7fe259345f976277d4a26
Cristo <christorl14@gmail.com>
2018-05-25 14:02:38 +0200

    Añado "adivina_el_numero"

---
+++ adivina_el_numero.py
@@ -0,0 +1,9 @@
+numero_a_adivinar = 9
+
+numero_que_dice_el_usuario = int(input("Adivina el número: "))
+
+if numero_a_adivinar == numero_que_dice_el_usuario:
+    print ("¡Ganaste!")
+
+else:
+    print ("Perdiste...")
